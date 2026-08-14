dc6d837b0f2291678c6091a9e45c5a416999696e
LinyuanLi0046 <liliny163@163.com>
2026-08-14 14:48:27 +0800

    revert: restore pipelined prefill loop

--- bench_welmv4_inplace_rope_npu.py
+++ bench_welmv4_inplace_rope_npu.py
@@ -413,12 +413,13 @@ def _candidate_apply_token_head_block_rope(
     )
 
 
-@triton.jit(do_not_specialize=["N"])
+@triton.jit(do_not_specialize=["num_token_blocks", "N"])
 def _candidate_welmv4_inplace_rope_prefill_kernel(
     q_ptr: tl.tensor,
     k_ptr: tl.tensor,
     position_ptr: tl.tensor,
     cos_sin_cache_ptr: tl.tensor,
+    num_token_blocks: int,
     N: int,
     q_token_stride: tl.constexpr,
     k_token_stride: tl.constexpr,
@@ -426,119 +427,125 @@ def _candidate_welmv4_inplace_rope_prefill_kernel(
     rope_dim: tl.constexpr,
     token_block: tl.constexpr,
     masked: tl.constexpr,
+    num_stages: tl.constexpr,
     num_q_heads: tl.constexpr,
 ):
     half_rope_dim: tl.constexpr = rope_dim // 2
     token_offsets = tl.arange(0, token_block)
     cos_offsets = tl.arange(0, half_rope_dim)
     sin_offsets = tl.arange(half_rope_dim, rope_dim)
-    block_id = tl.program_id(0)
-    token_base = block_id * token_block
-    token_mask = token_base + token_offsets < N
-    if masked:
-        position_ids = tl.load(
-            position_ptr + token_base + token_offsets,
-            mask=token_mask,
-            other=0,
-        ).to(tl.int32)
-        cos = tl.load(
-            cos_sin_cache_ptr
-            + position_ids[:, None] * rope_dim
-            + cos_offsets[None, :],
-            mask=token_mask[:, None],
-            other=0.0,
-            care_padding=False,
+    for block_id in tl.range(
+        tl.program_id(0),
+        num_token_blocks,
+        tl.num_programs(0),
+        num_stages=num_stages,
+    ):
+        token_base = block_id * token_block
+        token_mask = token_base + token_offsets < N
+        if masked:
+            position_ids = tl.load(
+                position_ptr + token_base + token_offsets,
+                mask=token_mask,
+                other=0,
+            ).to(tl.int32)
+            cos = tl.load(
+                cos_sin_cache_ptr
+                + position_ids[:, None] * rope_dim
+                + cos_offsets[None, :],
+                mask=token_mask[:, None],
+                other=0.0,
+                care_padding=False,
+            )
+            sin = tl.load(
+                cos_sin_cache_ptr
+                + position_ids[:, None] * rope_dim
+                + sin_offsets[None, :],
+                mask=token_mask[:, None],
+                other=0.0,
+                care_padding=False,
+            )
+        else:
+            position_ids = tl.load(
+                position_ptr + token_base + token_offsets
+            ).to(tl.int32)
+            cos = tl.load(
+                cos_sin_cache_ptr
+                + position_ids[:, None] * rope_dim
+                + cos_offsets[None, :],
+                care_padding=False,
+            )
+            sin = tl.load(
+                cos_sin_cache_ptr
+                + position_ids[:, None] * rope_dim
+                + sin_offsets[None, :],
+                care_padding=False,
+            )
+
+        k_data = (
+            k_ptr
+            + token_base * k_token_stride
+            + head_dim
+            - rope_dim
         )
-        sin = tl.load(
-            cos_sin_cache_ptr
-            + position_ids[:, None] * rope_dim
-            + sin_offsets[None, :],
-            mask=token_mask[:, None],
-            other=0.0,
-            care_padding=False,
-        )
-    else:
-        position_ids = tl.load(
-            position_ptr + token_base + token_offsets
-        ).to(tl.int32)
-        cos = tl.load(
-            cos_sin_cache_ptr
-            + position_ids[:, None] * rope_dim
-            + cos_offsets[None, :],
-            care_padding=False,
-        )
-        sin = tl.load(
-            cos_sin_cache_ptr
-            + position_ids[:, None] * rope_dim
-            + sin_offsets[None, :],
-            care_padding=False,
+        _candidate_apply_token_block_rope(
+            k_data,
+            token_offsets,
+            k_token_stride,
+            cos,
+            sin,
+            token_mask,
+            masked,
+            head_dim,
+            rope_dim,
         )
 
-    k_data = (
-        k_ptr
-        + token_base * k_token_stride
-        + head_dim
-        - rope_dim
-    )
-    _candidate_apply_token_block_rope(
-        k_data,
-        token_offsets,
-        k_token_stride,
-        cos,
-        sin,
-        token_mask,
-        masked,
-        head_dim,
-        rope_dim,
-    )
-
-    if masked:
-        for head_id in tl.static_range(0, num_q_heads):
+        if masked:
+            for head_id in tl.static_range(0, num_q_heads):
+                q_data = (
+                    q_ptr
+                    + token_base * q_token_stride
+                    + head_id * head_dim
+                    + head_dim
+                    - rope_dim
+                )
+                _candidate_apply_token_block_rope(
+                    q_data,
+                    token_offsets,
+                    q_token_stride,
+                    cos,
+                    sin,
+                    token_mask,
+                    masked,
+                    head_dim,
+                    rope_dim,
+                )
+        else:
             q_data = (
                 q_ptr
                 + token_base * q_token_stride
-                + head_id * head_dim
                 + head_dim
                 - rope_dim
             )
-            _candidate_apply_token_block_rope(
+            _candidate_apply_token_head_block_rope(
                 q_data,
                 token_offsets,
                 q_token_stride,
                 cos,
                 sin,
-                token_mask,
-                masked,
+                4,
                 head_dim,
                 rope_dim,
             )
-    else:
-        q_data = (
-            q_ptr
-            + token_base * q_token_stride
-            + head_dim
-            - rope_dim
-        )
-        _candidate_apply_token_head_block_rope(
-            q_data,
-            token_offsets,
-            q_token_stride,
-            cos,
-            sin,
-            4,
-            head_dim,
-            rope_dim,
-        )
-        _candidate_apply_token_head_block_rope(
-            q_data + 4 * head_dim,
-            token_offsets,
-            q_token_stride,
-            cos,
-            sin,
-            2,
-            head_dim,
-            rope_dim,
-        )
+            _candidate_apply_token_head_block_rope(
+                q_data + 4 * head_dim,
+                token_offsets,
+                q_token_stride,
+                cos,
+                sin,
+                2,
+                head_dim,
+                rope_dim,
+            )
 
 PROVIDERS = {
     "baseline": _baseline_welmv4_inplace_rope_kernel,
@@ -633,24 +640,22 @@ class Harness:
             ):
                 prefill_token_block = PREFILL_TOKEN_BLOCK_MIN
         use_blocked_prefill = prefill_token_block > 0
-        max_programs = self.num_vector_cores * PROGRAMS_PER_VECTOR_CORE
+        kernel = (
+            _candidate_welmv4_inplace_rope_prefill_kernel
+            if use_blocked_prefill
+            else PROVIDERS[provider]
+        )
         if use_blocked_prefill:
             work_items = (
                 triton.cdiv(n_tokens, prefill_token_block)
                 if prefill_masked
                 else n_tokens // prefill_token_block
             )
-            if work_items > max_programs:
-                use_blocked_prefill = False
-                work_items = n_tokens
         else:
             work_items = n_tokens
-        kernel = (
-            _candidate_welmv4_inplace_rope_prefill_kernel
-            if use_blocked_prefill
-            else PROVIDERS[provider]
+        num_programs = min(
+            work_items, self.num_vector_cores * PROGRAMS_PER_VECTOR_CORE
         )
-        num_programs = min(work_items, max_programs)
         q_stride = int(query.stride(0))
         k_stride = int(key.stride(0))
         q_heads_blocked = triton.next_power_of_2(NUM_Q_HEADS)
@@ -663,6 +668,7 @@ class Harness:
                     key,
                     positions,
                     self.cache,
+                    work_items,
                     n_tokens,
                     q_stride,
                     k_stride,
@@ -670,6 +676,7 @@ class Harness:
                     ROPE_DIM,
                     prefill_token_block,
                     prefill_masked,
+                    NUM_STAGES,
                     NUM_Q_HEADS,
                 )
             return kernel[(num_programs,)](
